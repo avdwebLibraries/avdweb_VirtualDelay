8df89c3bd2f8f25e18f285882e421c89ae6075a8
Albert van Dalen <albertfora@avdweb.nl>
2017-09-04 16:47:10 +0200

    Add files via upload

---
+++ Examples/Test_virtualDelay_simple.ino
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include "avdweb_VirtualDelay.h"
+
+const byte ledPin = 13;
+bool b;
+VirtualDelay singleDelay(millis);
+
+void setup() 
+{ pinMode(ledPin, OUTPUT);
+}
+
+void loop() 
+{ if(singleDelay.done(500)) digitalWrite(ledPin, b=!b); // repeat every 500ms
+}
+
