e4c5aad5ef73f0f9717bd5cc8df9abf4e2435ca5
Albert van Dalen <albertfora@avdweb.nl>
2017-09-08 15:00:33 +0200

    Add files via upload

---
+++ Test_virtualDelay1/Test_virtualDelay1.ino
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include "avdweb_VirtualDelay.h"
+
+const byte ledPin = 13;
+bool b;
+VirtualDelay singleDelay; // default = millis
+
+void setup() 
+{ pinMode(ledPin, OUTPUT);
+}
+
+void loop() 
+{ if(singleDelay.done(300, 1)) digitalWrite(ledPin, b=!b); // blink the onboard LED
+}
+
