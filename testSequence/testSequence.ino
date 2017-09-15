8cf988bde5ddf53c436b29055fc38f868431172c
Albert van Dalen <albertfora@avdweb.nl>
2017-09-15 09:52:30 +0200

    Add files via upload

---
+++ testSequence/testSequence.ino
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+#include <Streaming.h>
+#include "avdweb_VirtualDelay.h"
+
+VirtualDelay delay1, delay2, delay3;
+
+void setup() 
+{ Serial.begin(9600);
+  Serial << "\ntestSequence";
+}
+
+void loop() 
+{ if(delay1.elapsed()) // this sequence has a deadlock
+  { Serial << "\ndelay1 200ms " << millis(); 
+    delay2.start(100);
+  }
+  if(delay2.elapsed())
+  { Serial << "\ndelay2 100ms " << millis(); 
+    delay3.start(400);
+  }
+  if(delay3.elapsed())
+  { Serial << "\ndelay3 400ms " << millis();
+    delay1.start(200); 
+  }
+  DO_ONCE(delay1.start(200)); // breaks the deadlock, you can start with any delay object you want e.g. delay2.start(0); 
+}
+
+
+
