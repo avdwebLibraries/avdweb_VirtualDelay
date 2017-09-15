b4cc5b89ff53e078a897773d88ae2c13471f79e2
Albert van Dalen <albertfora@avdweb.nl>
2017-09-15 09:51:44 +0200

    Add files via upload

---
+++ testOneShot/testOneShot.ino
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+#include <Streaming.h>
+#include "avdweb_VirtualDelay.h"
+
+VirtualDelay delay1;
+
+void setup() 
+{ Serial.begin(9600);
+  Serial << "\ntestOneShot 2s\n"; 
+}
+
+void loop() 
+{ DO_ONCE(delay1.start(2000)) // do only one time in the loop   
+  if(delay1.elapsed()) Serial << millis() << "ms" ; 
+}
+
+
+
