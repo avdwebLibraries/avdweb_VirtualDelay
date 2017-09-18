1e2432cea89f7be12e6ca1de5fc72f7d06e56eb7
Albert van Dalen <albertfora@avdweb.nl>
2017-09-18 09:56:59 +0200

    Add files via upload

--- Test_virtualDelay_LED/Test_virtualDelay_LED.ino
+++ Test_virtualDelay_LED/Test_virtualDelay_LED.ino
@@ -1,20 +1,16 @@
 #include <Arduino.h>
 #include "avdweb_VirtualDelay.h"
-#include <Streaming.h>
 
 const byte ledPin = 13;
 bool b;
-int i;
 VirtualDelay singleDelay; // default = millis
 
 void setup() 
 { pinMode(ledPin, OUTPUT);
-  Serial.begin(9600);
 }
 
 void loop() 
 { singleDelay.start(400); // calls while running are ignored
   if(singleDelay.elapsed()) digitalWrite(ledPin, b=!b); // blink the onboard LED 400ms, 400ms off
-  Serial << endl << i++; // since the cpu is not being blocked, printing goes fast
 }
 
