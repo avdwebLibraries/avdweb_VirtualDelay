74f82ea03c912636e323f7fed0ac0a78bd365b26
Albert van Dalen <albertfora@avdweb.nl>
2017-10-23 16:16:46 +0200

    Add files via upload

---
+++ Test_micros/Test_micros.ino
@@ -0,0 +1,16 @@
+#include "avdweb_VirtualDelay.h"
+
+VirtualDelay delay_us(micros);
+
+void setup() 
+{ Serial.begin(9600);
+}
+
+void loop() 
+{ DO_ONCE ( delay_us.start(1000000)) // 1s
+  if(delay_us.elapsed()) Serial.print(micros());
+}
+
+
+
+
