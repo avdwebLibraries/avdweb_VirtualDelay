caa30ce69b63155855c212a4877035184c99ce53
TADT <thienandangthanh@gmail.com>
2018-12-03 23:18:20 +0700

    Format files for better reading

--- Test_micros/Test_micros.ino
+++ Test_micros/Test_micros.ino
@@ -2,15 +2,9 @@
 
 VirtualDelay delay_us(micros);
 
-void setup() 
-{ Serial.begin(9600);
+void setup() { Serial.begin(9600); }
+
+void loop() {
+  DO_ONCE(delay_us.start(1000000))  // 1s
+  if (delay_us.elapsed()) Serial.print(micros());
 }
-
-void loop() 
-{ DO_ONCE ( delay_us.start(1000000)) // 1s
-  if(delay_us.elapsed()) Serial.print(micros());
-}
-
-
-
-
--- Test_virtualDelay_LED/Test_virtualDelay_LED.ino
+++ Test_virtualDelay_LED/Test_virtualDelay_LED.ino
@@ -3,14 +3,12 @@
 
 const byte ledPin = 13;
 bool b;
-VirtualDelay singleDelay; // default = millis
+VirtualDelay singleDelay;  // default = millis
 
-void setup() 
-{ pinMode(ledPin, OUTPUT);
+void setup() { pinMode(ledPin, OUTPUT); }
+
+void loop() {
+  singleDelay.start(400);  // calls while running are ignored
+  if (singleDelay.elapsed())
+    digitalWrite(ledPin, b = !b);  // blink the onboard LED 400ms, 400ms off
 }
-
-void loop() 
-{ singleDelay.start(400); // calls while running are ignored
-  if(singleDelay.elapsed()) digitalWrite(ledPin, b=!b); // blink the onboard LED 400ms, 400ms off
-}
-
